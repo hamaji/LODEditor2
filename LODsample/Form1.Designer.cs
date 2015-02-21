5979a1ed893c9161d1e912cb20d5e42efdb25fa1
hamaji <k.hamaji@gmail.com>
2015-02-21 18:59:47 +0900

    新規テスト

---
+++ LODsample/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace LODsample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtEndpoint = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(27, 223);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(638, 175);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(480, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "エンドポイントブラウズ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtEndpoint
+            // 
+            this.txtEndpoint.Location = new System.Drawing.Point(27, 29);
+            this.txtEndpoint.Name = "txtEndpoint";
+            this.txtEndpoint.Size = new System.Drawing.Size(426, 22);
+            this.txtEndpoint.TabIndex = 2;
+            this.txtEndpoint.Text = "http://ja.dbpedia.org/sparql";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(27, 67);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(638, 123);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 435);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtEndpoint);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "LODsample";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtEndpoint;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ LODsample/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+namespace LODsample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            txtEndpoint.Text = "http://ja.dbpedia.org/sparql?";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //エンドポイントURL作成
+            var url = URL作成();
+            
+            //エンドポイントのアクセス
+            // HTTPアクセス
+            var req = WebRequest.Create(url);
+            req.Headers.Add("Accept-Language:ja,en-us;q=0.7,en;q=0.3");
+            var res = req.GetResponse();
+            //エンドポイントの構造の表示
+
+
+
+        }
+
+        private string URL作成()
+        {
+            var endpoint = txtEndpoint.Text;
+            var arg = "東京都";
+            var format = "text";
+
+            // 実際のSPARQLクエリ文。URLを打つのがめんどい場合はPREFIX使おう
+        // 今回は関数の引数に実際の値となるもの（東京都とか岩手県とか）を渡す感じで
+        // また、Abstractの項目の値のみを取得しています。全部取得する場合は?pとかで
+        var query = "select distinct * where {"+ " <" + "http://ja.dbpedia.org/resource/" +arg  +"> < "+"http://dbpedia.org/ontology/abstract> ?o . }";
+
+        var urlen = System.Web.HttpUtility.UrlEncode(query);
+            return endpoint + "query=" +urlen +"&format=" +format;
+        }
+
+    }
+}
